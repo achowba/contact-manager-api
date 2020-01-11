90e1a7a874403e3624c35e738bda8bdf5c0789ad
achowba <achowba>
2020-01-11 10:36:13 +0400

    add validation functions

---
+++ server/helpers/validation.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+function validateContact (contact) {
+    const contactSchema = {
+        _id: Joi.objectId().required(),
+        firstName: Joi.string().min(1).required(),
+        lastName: Joi.string().min(1).required(),
+        phoneNumber: Joi.string().min(5).required(),
+        email: Joi.string().min(6).required,
+        contactImage: Joi.string(),
+        createdOn: Joi.string(),
+        createdBy: Joi.objectId().required(),
+    }
+
+    return Joi.validate(contact, contactSchema);
+}
+
+function validateUser(user) {
+    const userSchema = {
+        _id: Joi.objectId().required(),
+        username: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(6).max(50).required(),
+        password: Joi.string().min(6).max(50).required(),
+    }
+
+    return Joi.validate(contact, contactSchema);
+}
+
+function validateUserLogin(user) {
+    const userSchema = {
+        email: Joi.string().min(6).max(50).required(),
+        password: Joi.string().min(6).max(50).required(),
+    }
+
+    return Joi.validate(contact, contactSchema);
+}
+
+module.exports = {
+    validateContact,
+    validateUser,
+    validateUserLogin
+}
